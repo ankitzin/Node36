2171027be3156af16889e1d3be97fc4a958cb363
Ankit <ankit.zin279@gmail.com>
2020-04-02 01:57:06 +0530

    final mvc with enhancement

--- app.js
+++ app.js
@@ -3,23 +3,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const errorController = require('./controllers/error')
 
-app.set('view engine','ejs')
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorController.error404);
+app.use(errorController.get404);
 
-app.listen(3000,()=>{
-    console.log("running on 3000");
-    
-});
+app.listen(3000);
--- models/product.js
+++ models/product.js
@@ -1,15 +1,40 @@
-const products = []
+const fs = require('fs');
+const path = require('path');
 
-module.exports = class product {
-    constructor(t){
-        this.title = t;
-    }
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-    save(){
-        products.push(this);
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
+  });
+};
 
-    static fetchAll(){
-        return products;
-    }
-}
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
