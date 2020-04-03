c065ead3e43382f989b5c75f4ef0ab980f9d02c9
Ankit <ankit.zin279@gmail.com>
2020-04-03 06:02:38 +0530

    mysql2 database works done TODO

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,41 +2,48 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll().then(([rows,fieldData])=>{
     res.render('shop/product-list', {
-      prods: products,
+      prods: rows,
       pageTitle: 'All Products',
       path: '/products'
     });
   });
 };
-
+ 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+  Product.findById(prodId)
+  .then(([product,fieldData])=>{
     res.render('shop/product-detail', {
-      product: product,
+      product: product[0],
       pageTitle: product.title,
       path: '/products'
     });
-  });
+  })
+  .catch(err => console.log(err)
+  );
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll().then(([rows,fieldData]) =>{
     res.render('shop/index', {
-      prods: products,
+      prods: rows,
       pageTitle: 'Shop',
       path: '/'
     });
-  });
+  }).catch(err =>{
+    console.log(err);
+    
+  })
+    
 };
 
 exports.getCart = (req, res, next) => {
   Cart.getCart(cart => {
-    Product.fetchAll(products => {
+    Product.fetchAll().then(([rows,fieldData])=>{
       const cartProducts = [];
-      for (product of products) {
+      for (product of rows) {
         const cartProductData = cart.products.find(
           prod => prod.id === product.id
         );
@@ -49,16 +56,24 @@ exports.getCart = (req, res, next) => {
         pageTitle: 'Your Cart',
         products: cartProducts
       });
+
+    }).catch(err =>{
+      console.log(err);
     });
+      
   });
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect('/cart');
+  Product.findById(prodId)
+  .then(([rows,fieldData])=>{
+    Cart.addProduct(prodId, rows.price);
+    res.redirect('/cart');
+  })
+  .catch(err => console.log(err));
+  
+  
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
